e5a61e069a8756237e8ee563fa613a9219d3dfbc
sherzod <sheraliyevs147@gmail.com>
2026-01-20 16:21:42 +0500

    added

---
+++ app.js
@@ -0,0 +1,17 @@
+//foydalanuvchimalumot ollish
+confirm("ro'yhatdan o'tmoqchimisiz?") // sstring
+prompt("ismingizni kiriting") // boolean
+alert("done") // foydalanuvchi uchin
+console.log("hello") // developer uchin
+
+
+//o'zgaruvchilar
+let userAge = 15
+console.log(userAge);
+ userAge = 16
+console.log(userAge)
+
+//o'zgarmaslar
+const userBirthYear = 2008
+userBirthYear = 2009
+console.log(userBirthYear);
