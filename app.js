fdc332e64b44221ccfc08416a0480a82d8146281
sherzod <sheraliyevs147@gmail.com>
2026-01-24 16:12:52 +0500

    added

--- app.js
+++ app.js
@@ -1,17 +1,21 @@
-//foydalanuvchimalumot ollish
-confirm("ro'yhatdan o'tmoqchimisiz?") // sstring
-prompt("ismingizni kiriting") // boolean
-alert("done") // foydalanuvchi uchin
-console.log("hello") // developer uchin
+// //foydalanuvchimalumot ollish
+// confirm("ro'yhatdan o'tmoqchimisiz?") // sstring
+// prompt("ismingizni kiriting") // boolean
+// alert("done") // foydalanuvchi uchin
+// console.log("hello") // developer uchin
 
 
-//o'zgaruvchilar
-let userAge = 15
-console.log(userAge);
- userAge = 16
-console.log(userAge)
+// //o'zgaruvchilar
+// let userAge = 15
+// console.log(userAge);
+//  userAge = 16
+// console.log(userAge)
 
-//o'zgarmaslar
-const userBirthYear = 2008
-userBirthYear = 2009
-console.log(userBirthYear);
+// //o'zgarmaslar
+// const userBirthYear = 2008
+// userBirthYear = 2009
+// console.log(userBirthYear);
+
+const username = prompt("ism")
+const userAge2 = prompt("yosh")
+alert(username + ",siz " + $ (2026 - userAge2)  + " yoshda")
